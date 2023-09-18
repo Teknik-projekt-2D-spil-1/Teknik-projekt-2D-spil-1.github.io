5b7388bcbbbcaceb53c711a7191ee524a50d8385
LVTE1808 <lasseelmsted@gmail.com>
2023-09-18 14:46:45 +0200

    character movement update
    character movement update

--- source/KinematicBody2D.gd
+++ source/KinematicBody2D.gd
@@ -1,46 +1,20 @@
 extends KinematicBody2D
 
-var max_speed = 3000
-var ACCELERATION = 1500
-var friction = 1200
-var gravity = 2000
-var jump_velocity = -800
-var axis = Vector2.ZERO
+var velocity = Vector2.ZERO
 
+#ganger ikke med velocity med delta, da det allerede er indbygget i funktionen 
 func _physics_process(delta):
-	move(delta)
-
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = jump_velocity
-
-func get_input_axis():
-	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	return axis.normalized()
-	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-
-func move(delta):
-	axis = get_input_axis()
+	velocity.y += 3
+	#den linje over svarer til gravity
 	
-	if axis == Vector2.ZERO:
-		apply_friction(friction * delta)
-	
+	if Input.action_is_pressed("move_right"):
+		velocity.x = 60
+	elif Input.action_is_pressed("move_left"):
+		velocity.x = -60
 	else:
-		apply_movement(axis * ACCELERATION * delta)
-
-	move_and_slide()
-
-
-func apply_friction(amount):
-	if velocity.length() > amount:
-		velocity -= velocity.normalized() * amount
+		velocity.x = 0
 	
-	else:
-		velocity = Vector2.ZERO
-
-
-func apply_movement(ACCELERATION):
-	velocity += ACCELERATION
-	velocity = velocity.limit_length(max_speed)
+	if Input.is_action_pressed("jump"):
+		velocity.y = -150
+	
+	velocity = move_and_slide(velocity)
