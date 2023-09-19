42d2297de46b5f4fe3b17db67f5d27f5b6673f10
LVTE1808 <lasseelmsted@gmail.com>
2023-09-19 10:08:54 +0200

    lot a trouble

--- source/KinematicBody2D.gd
+++ source/KinematicBody2D.gd
@@ -1,18 +1,11 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var jump = -16
-
-
-
-
 
 #ganger ikke med velocity med delta, da det allerede er indbygget i funktionen 
 func _physics_process(_delta):
 	velocity.y += 3
-	#den linje over svarer til gravity
-
-
+#den linje over svarer til gravity
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = 60
 	elif Input.is_action_pressed("ui_left"):
@@ -20,8 +13,8 @@ func _physics_process(_delta):
 	else:
 		velocity.x = 0
 
-	if Input.is_action_pressed("ui_up") and is_on_floor():
-		velocity.y = jump
+	if Input.is_action_pressed("jump"):
+		velocity.y = -16
 	#-y svarer til opad
 	
 	velocity = move_and_slide(velocity)
