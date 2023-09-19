a36f64f17bbdfd65b5a8b3dc74409c806d429c87
LVTE1808 <lasseelmsted@gmail.com>
2023-09-19 10:29:28 +0200

    rawr

--- source/KinematicBody2D.gd
+++ source/KinematicBody2D.gd
@@ -6,15 +6,16 @@ var velocity = Vector2.ZERO
 func _physics_process(_delta):
 	velocity.y += 3
 #den linje over svarer til gravity
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		velocity.x = 60
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("move_left"):
 		velocity.x = -60
 	else:
 		velocity.x = 0
 
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -16
 	#-y svarer til opad
+	
 	
 	velocity = move_and_slide(velocity)
